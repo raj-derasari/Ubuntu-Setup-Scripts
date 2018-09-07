753c3323b992beafa871d842a019f9407a24af82
raj-derasari <raj.derasari@gmail.com>
2018-09-07 19:56:17 +0530

    Create final-setup.sh

---
+++ final-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rm -rf ~/SetupScript
+sudo apt-get update
+sudo apt-get install -y git
+sudo apt-get install -fy
+mkdir -p ~/SetupScript
+cd ~/SetupScript
+git clone https://github.com/raj-derasari/My-Ubuntu-Setup-Scripts
+cd My-Ubuntu-Setup-Scripts
+echo "About to bash master.sh now"
+chmod 777 master.sh
+reset; bash master.sh -C -D -f configs/config_full.sh > ~/Desktop/send_rd.txt
+read -p "Looking fine? Exec without dry-run? (Enter for yes, warna anything else) - " prom
+if test "$prom" = ""; then
+	reset; 
+	bash master.sh -C -f configs/config_full.sh
+else
+	exit 127
+fi
