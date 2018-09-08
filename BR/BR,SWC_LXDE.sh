d7f0e0c57dd96c9e9437f4aa495516f504947b89
raj-derasari <raj.derasari@gmail.com>
2018-09-09 01:12:49 +0530

    Add files via upload

--- BR/BR,SWC_LXDE.sh
+++ BR/BR,SWC_LXDE.sh
@@ -9,6 +9,8 @@ if test "$1" = "-D"; then
 	dry_echo="echo "
 fi
 
+br_purge="$dry_echo sudo apt-get purge -y "
+
 if [ -z $Master_RemoveBloatware ]; then
 	## variable wasn't defined
 	echo "Welcome to the LUbuntu fixer-setup script."
@@ -16,7 +18,12 @@ else
 	log $INFO "lubuntu-bloatremove was called"
 fi
 ## literally bloatware, in my opinion
-help="Removing the following packages:
+splash_msg="-------------------------------------
+Removing packages, followed by softwares:"
+echo $splash_msg
+
+# pkg to remove, remove lines or add packages below with a \\ at the end, to uninstall
+pkgs="
 gnome-disk-utility \
 gnome-mplayer \
 gnome-mpv \
@@ -27,10 +34,11 @@ mtools \
 mtpaint \
 mtr-tiny \
 simple-scan \
-ubuntu-release-upgrader-gtk \
-xfburn
--------------------------------------
-Followed by
+ubuntu-release-upgrader-gtk \ 
+xfburn "
+
+# pkg to remove, same as above
+sw="
 abiword* \
 alsa* \
 audacious* \
@@ -39,33 +47,15 @@ pidgin* \
 sylpheed* \
 transmission*
 "
-echo $help
 
-$dry_echo sudo apt-get purge -y \
-gnome-disk-utility \
-gnome-mplayer \
-gnome-mpv \
-gpicview \
-guvcview \
-leafpad \
-mtools \
-mtpaint \
-mtr-tiny \
-simple-scan \
-ubuntu-release-upgrader-gtk \
-xfburn
-
-$dry_echo sudo apt-get purge -y  \
-abiword* \
-alsa* \
-audacious* \
-gnumeric* \
-pidgin* \
-sylpheed* \
-transmission*
+$br_purge $pkgs
+$br_purge $sw
 
 $dry_echo sudo apt-get install -fy
-# to fix mute button problems
+
+# to fix mute button problems in Lubuntu
 $dry_echo sudo apt-get install alsa-utils
-sed -i 's/amixer -q sset Master toggle/amixer -D pulse set Master toggle/g' ~/.config/openbox/lubuntu-rc.xml && openbox --reconfigure
+
+# makes the mute button TOGGLE mute/unmute instead of FORCING mute every time its pressed
+$dry_echo sed -i 's/amixer -q sset Master toggle/amixer -D pulse set Master toggle/g' ~/.config/openbox/lubuntu-rc.xml && openbox --reconfigure
 
