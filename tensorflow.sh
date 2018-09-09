f8b75aea5e36a474d374634cd7c3eb6884abd226
raj-derasari <raj.derasari@gmail.com>
2018-09-09 22:35:02 +0530

    Update tensorflow.sh

--- tensorflow.sh
+++ tensorflow.sh
@@ -266,6 +266,8 @@ if [[ -z `which bazel` ]]; then
 	echo "bazel not found, installing bazel by apt-get"
 	log $INFO "bazel: Installing from this script"
 	echo "installing bazel-dependencies"
+	
+	echo "DE:" $dry_echo
 	$dry_echo sudo apt-get install -y build-essential cmake git python${PV}-dev python${PV}-distutls pylint libcupti-dev curl
 	if [ $DRY_MODE -eq 1 ]; then
 		echo "Add apt-repositories for bazel"
