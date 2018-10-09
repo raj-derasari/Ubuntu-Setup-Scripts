bccd5ed787fcb0758d232a9d0c196e962cc34ac5
raj derasari <raj.derasari@gmail.com>
2018-10-09 23:22:59 +0530

    bug fixes

--- other/themes_remove.sh
+++ other/themes_remove.sh
@@ -1,19 +1,5 @@
 # safe to purge
-sudo apt-get purge  --auto-remove adwaita-icon-theme
-sudo apt-get purge  --auto-remove dmz-cursor-theme
-sudo apt-get purge  --auto-remove gnome-icon-theme gtk-update-icon-cache
-sudo apt-get purge  --auto-remove humanity-icon-theme
-# uninstalls gnome-disk-utility but just replace it w gparted lol
+sudo apt-get purge  --auto-remove adwaita-icon-theme dmz-cursor-theme gnome-icon-theme gtk-update-icon-cache humanity-icon-theme hicolor-icon-theme
+ just replace it w gparted lol
 #sudo apt-get purge  --auto-remove sound-theme-freedesktop
-sudo apt-get purge  --auto-remove  plymouth-theme-lubuntu-logo plymouth-theme-lubuntu-text plymouth-label
-
-
-## risky
-sudo apt-get purge hicolor-icon-theme
-
-
-
-sudo apt-get purge adwaita-icon-theme dmz-cursor-theme gnome-icon-theme hicolor-icon-theme humanity-icon-theme plymouth-theme-lubuntu-logo plymouth-theme-lubuntu-text sound-theme-freedesktop
-
-# in case you lose the icons of sometihing
-sai lubuntu-artwork
+# sudo apt-get purge  --auto-remove  plymouth-theme-lubuntu-logo plymouth-theme-lubuntu-text plymouth-label
