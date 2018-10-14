029d78ef2b9fce586d63e523a5be82c496cf23bf
Raj Derasari <raj.derasari@gmail.com>
2018-10-14 07:43:31 +0530

    more

--- other/themes_remove.sh
+++
@@ -1,3 +0,0 @@
-# safe to purge
-sudo apt-get purge --auto-remove adwaita-icon-theme dmz-cursor-theme gnome-icon-theme gtk-update-icon-cache humanity-icon-theme hicolor-icon-theme
-#plymouth-theme-lubuntu-logo plymouth-theme-lubuntu-text plymouth-label
