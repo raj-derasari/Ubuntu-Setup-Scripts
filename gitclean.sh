dfd6651c1bdcb2a3975172f80a28149a484cd5ee
Raj <raj.derasari@gmail.com>
2018-10-16 15:44:16 +0530

    plsdontgetdeleted

--- gitclean.sh
+++ gitclean.sh
@@ -1,8 +1,8 @@
 _git_remove(){
 	git filter-branch --index-filter "git rm --cached --ignore-unmatch $1" -- --all;
-	 rm -Rf .git/refs/original; rm -Rf .git/logs/; git gc --aggressive --prune=now
+	rm -Rf .git/refs/original; rm -Rf .git/logs/; git gc --aggressive --prune=now
 	## verification
-	 git count-objects -v
+	git count-objects -v
 }
 
 z=`git verify-pack -v .git/objects/pack/pack-*.idx | sort -k 3 -n | tail -10 | cut -d' ' -f1`
@@ -15,5 +15,9 @@ done
 # rm -Rf .git/refs/original; rm -Rf .git/logs/; git gc --aggressive --prune=now
 
 _git_remove "*Templates.zip*"
-#_git_remove "*.deb*"
-##_git_remove '*.deb*'
+_git_remove "*.deb*"
+_git_remove '*nasm_tensorflow*'
+_git_remove '*.bzl*'
+_git_remove '*convolutional*'
+_git_remove '*.log*'
+_git_remove '*veracrypt*'
