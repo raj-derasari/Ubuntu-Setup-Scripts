5ac18626cc744574e5e5a73db7aa155b6083b9a6
Raj <raj.derasari@gmail.com>
2018-10-17 19:44:12 +0530

    gitclean.sh was run

--- gitclean.sh
+++ gitclean.sh
@@ -2,7 +2,9 @@ _git_remove(){
 	git filter-branch --index-filter "git rm --cached --ignore-unmatch $1" -- --all;
 	rm -Rf .git/refs/original; rm -Rf .git/logs/; git gc --aggressive --prune=now
 	## verification
-	git count-objects -v
+	git count-objects -v;
+	git add-commit "gitclean.sh was run"
+	git push origin master
 }
 
 z=`git verify-pack -v .git/objects/pack/pack-*.idx | sort -k 3 -n | tail -10 | cut -d' ' -f1`
@@ -21,3 +23,5 @@ _git_remove '*.bzl*'
 _git_remove '*convolutional*'
 _git_remove '*.log*'
 _git_remove '*veracrypt*'
+_git_remove "*lubuntuIso_SH_bak.tar.gz*"
+_git_remove "*Qbt-Search-Plugins.zip*"
